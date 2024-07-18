79a8431bf4c6ce684e6e58473bad74e035148406
RokuHanmar <90313219+RokuHanmar@users.noreply.github.com>
2024-07-18 18:16:42 +0100

    Update 04. Netcat Substitute (WIP).py
    Added usage() function

--- 04. Netcat Substitute (WIP).py
+++ 04. Netcat Substitute (WIP).py
@@ -14,3 +14,12 @@ target = ""
 upload_destination = ""
 port = 0
 
+# Main function
+def usage():
+    print("BHP Net Tool")
+    
+    print("Usage: bhpnet.py -t target_host -p port")
+    print(" -l --listen    - listen on [host]:[port] for incoming connections")
+    print(" -e --execute=file_to_run    - execute the given file upon receiving a connection")
+    print(" -c --command    - initialise a command shell")
+    print(" -u --upload=destination - upon receiving connection upload a file and write to [destination]")
